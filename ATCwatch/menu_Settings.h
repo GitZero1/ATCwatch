780d0ebd9433cbe8efd739b053cd22af5ac749a0
GitZerone <zeroonegames89@gmail.com>
2021-11-11 15:15:48 -0500

    started settings screen changes

--- ATCwatch/menu_Settings.h
+++ ATCwatch/menu_Settings.h
@@ -25,7 +25,7 @@ class SettingsScreen : public Screen
   public:
     virtual void pre()
     {
-      set_swipe_enabled(true);
+      //set_swipe_enabled(true);
 
       //Create bg style
       static lv_style_t style_bg;
@@ -62,7 +62,7 @@ class SettingsScreen : public Screen
       lv_obj_t *tabview;
       tabview = lv_tabview_create(lv_scr_act(), NULL);
       lv_obj_align(tabview, nullptr, LV_ALIGN_CENTER, 0, 0);
-      lv_tabview_set_style(tabview, LV_TABVIEW_STYLE_BG, &style_btn);
+      lv_tabview_set_style(tabview, LV_TABVIEW_STYLE_BG, &style_bg);
       lv_tabview_set_style(tabview,LV_TABVIEW_STYLE_BTN_TGL_PR, &style_btn_sel);
       lv_tabview_set_style(tabview,LV_TABVIEW_STYLE_BTN_TGL_REL, &style_btn_sel);
       lv_tabview_set_style(tabview,LV_TABVIEW_STYLE_BTN_PR, &style_btn);
@@ -75,50 +75,57 @@ class SettingsScreen : public Screen
       lv_obj_set_style(tab2, &style_btn);
 
       //TAB ONE ###########################################################
-      //Create a list in tab 1
-      lv_obj_t * list1 = lv_list_create(tab1, NULL);
-      lv_obj_set_size(list1, 200, 154);
-      lv_obj_align(list1, NULL, LV_ALIGN_CENTER, 0, 0);
-      //Style the list
-      lv_obj_set_style(list1, &style_btn);
-      lv_list_set_style(list1, LV_LIST_STYLE_BTN_TGL_REL, &style_btn);
-      lv_list_set_style(list1, LV_LIST_STYLE_BTN_REL, &style_btn);
-
-      //Add buttons to the list
-      lv_obj_t * list_btn;
       
 
-      //button1
-      list_btn = lv_list_add_btn(list1, LV_SYMBOL_REFRESH, "Reboot");
-      lv_obj_set_event_cb(list_btn, lv_event_handler);
+      //power button
+      pwr_btn = lv_btn_create(tab1, nullptr); //create button
+      lv_obj_set_height(pwr_btn,45); //set button height
+      lv_btn_set_fit2(pwr_btn,LV_FIT_FILL, LV_FIT_NONE); //set button width
+      lv_obj_align(pwr_btn, tab1, LV_ALIGN_CENTER, 0, -50); //set button location
+      pwr_btn_label = lv_label_create(pwr_btn, nullptr); //create button label
+      lv_label_set_text(pwr_btn_label,"Power"); //set button label text
+      set_btn_style(pwr_btn); //set button style
+      lv_obj_set_event_cb(pwr_btn, lv_event_handler); //set event handler
 
-      //button2
-      list_btn = lv_list_add_btn(list1, LV_SYMBOL_POWER, "Off");
-      lv_obj_set_event_cb(list_btn, lv_event_handler);
 
-      //button3
-      list_btn = lv_list_add_btn(list1, LV_SYMBOL_DOWNLOAD, "Bootloader");
-      lv_obj_set_event_cb(list_btn, lv_event_handler);
+      //Reboot Button
+      reboot_btn = lv_btn_create(tab1, nullptr);  //create button
+      lv_obj_set_height(reboot_btn,45); //set button height
+      lv_btn_set_fit2(reboot_btn,LV_FIT_FILL, LV_FIT_NONE); //set button width
+      lv_obj_align(reboot_btn, tab1, LV_ALIGN_CENTER, 0, 0); //set button location
+      reboot_btn_label = lv_label_create(reboot_btn, nullptr); //create button label
+      lv_label_set_text(reboot_btn_label,"Reboot"); //set button label text
+      set_btn_style(reboot_btn);  //set button style
+      lv_obj_set_event_cb(reboot_btn, lv_event_handler); //set event handler
+
+      // BootLoader Button
+      bootload_btn = lv_btn_create(tab1, nullptr);  //create button
+      lv_obj_set_height(bootload_btn,45); //set button height
+      lv_btn_set_fit2(bootload_btn,LV_FIT_FILL, LV_FIT_NONE); //set button width
+      lv_obj_align(bootload_btn, tab1, LV_ALIGN_CENTER, 0, 50); //set button location
+      bootload_btn_label = lv_label_create(bootload_btn, nullptr); //create button label
+      lv_label_set_text(bootload_btn_label,"Bootloader"); //set button label text
+      set_btn_style(bootload_btn);  //set button style
+      lv_obj_set_event_cb(bootload_btn, lv_event_handler); //set event handler
 
 
       // TAB TWO #########################################################
-      //Create a list in tab 2
-      lv_obj_t * list2 = lv_list_create(tab2, NULL);
-      lv_obj_set_size(list2, 200, 154);
-      lv_obj_align(list2, NULL, LV_ALIGN_CENTER, 0, 0);
-      //style list2
-      lv_obj_set_style(list2, &style_bg);
-      lv_list_set_style(list2, LV_LIST_STYLE_BTN_TGL_REL, &style_btn);
-      lv_list_set_style(list2, LV_LIST_STYLE_BTN_REL, &style_btn);
 
-       //button1
-      list_btn = lv_list_add_btn(list2, LV_SYMBOL_IMAGE, "Style");
-      lv_obj_set_event_cb(list_btn, lv_event_handler);
       
-      //button2
-      list_btn = lv_list_add_btn(list2, LV_SYMBOL_EYE_OPEN, "Brightness");
-      lv_obj_set_event_cb(list_btn, lv_event_handler);
 
+      //sample button
+      sample_btn = lv_btn_create(tab2, nullptr); //create button
+      lv_obj_set_height(sample_btn,45); //set button height
+      lv_btn_set_fit2(sample_btn,LV_FIT_FILL, LV_FIT_NONE); //set button width
+      lv_obj_align(sample_btn, tab2, LV_ALIGN_CENTER, 0, -50); //set button location
+      sample_btn_label = lv_label_create(sample_btn, nullptr); //create button label
+      lv_label_set_text(sample_btn_label,"Sample"); //set button label text
+      set_btn_style(sample_btn); //set button style
+      lv_obj_set_event_cb(sample_btn, lv_event_handler); //set event handler
+
+      //TODO add buttons to select color options and brightness
+      // with apply and cancel as well. 
+      
     }
 
     
@@ -142,19 +149,25 @@ class SettingsScreen : public Screen
     virtual void lv_event_class(lv_obj_t * object, lv_event_t event)
     {
       if (event == LV_EVENT_SHORT_CLICKED) {
-        String btnName = lv_list_get_btn_text(object);
-        
-        if(btnName == "Reboot"){change_screen((Screen*)&rebootScreen);}
-        else if(btnName == "Off"){change_screen((Screen*)&offScreen);}
-        else if(btnName == "Bootloader"){change_screen((Screen*)&updateScreen);}
-        else if(btnName == "Style"){change_screen((Screen*)&settingsColorScreen);}
-        else if(btnName == "Brightness"){change_screen((Screen*)&settingsBrightnessScreen);}
+        if(object == reboot_btn){change_screen((Screen*)&rebootScreen);}
+        else if(object == pwr_btn){change_screen((Screen*)&offScreen);}
+        else if(object == bootload_btn){change_screen((Screen*)&updateScreen);}
+        //else if(object == "Style"){change_screen((Screen*)&settingsColorScreen);}
+        //else if(object == "Brightness"){change_screen((Screen*)&settingsBrightnessScreen);}
         
       }
     }
 
   private:
-  
+  //tab1 btns
+  lv_obj_t *reboot_btn, *pwr_btn, *bootload_btn;
+  //tab1 labels
+  lv_obj_t *pwr_btn_label, *reboot_btn_label, *bootload_btn_label;
+
+  //tab2 btns
+  lv_obj_t *sample_btn, *left_btn, *right_btn;
+  //tab2 labels
+  lv_obj_t *sample_btn_label;
 };
 
 SettingsScreen settingsScreen;
