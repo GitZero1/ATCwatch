4fcd6f3de6dd989f25e9f8cbe38b1944514e82ea
GitZerone <zeroonegames89@gmail.com>
2021-12-11 14:57:08 -0500

    SN80 added for easy testing, not finished

--- ATCwatch/pinout.h
+++ ATCwatch/pinout.h
@@ -7,10 +7,14 @@
 #ifndef _PINOUT_
 #define _PINOUT_
 
-#define P8WATCH//Enable or Disable this line to set for Pinetime or P8 Watch, more pinouts to added here
+//Enable or Disable this line to set for Pinetime, P8 Watch, or SN80. more pinouts to added here
+
+#define P8WATCH
+//#define PineTime
+//#define SN80
+
 
 #ifdef P8WATCH
-
 //display
 #define LCD_SCK      2
 #define LCD_SDI      3
@@ -28,8 +32,58 @@
 //Touchscreen
 #define TP_SDA       6
 #define TP_SCL       7
-//#define TP_RESET     10 //PinetTime
-#define TP_RESET       13 // P8 Watch
+#define TP_RESET     13 
+#define TP_INT       28
+
+//Accl Sensor
+#define BMA421_SDA       6
+#define BMA421_SCL       7
+#define BMA421_INT       8
+//#define SWITCH_X_Y
+
+//HeartRate Sensor
+#define HRS3300_SDA       6
+#define HRS3300_SCL       7
+#define HRS3300_TEST      30
+
+//Battery
+#define CHARGE_INDICATION -1  
+#define POWER_INDICATION  19
+#define BATTERY_VOLTAGE   31
+#define POWER_CONTROL     24
+
+//InputOutput
+#define STATUS_LED        27
+#define VIBRATOR_OUT      16
+#define PUSH_BUTTON_IN    17  
+#define PUSH_BUTTON_OUT   -1  
+
+//Backlight
+#define LCD_BACKLIGHT_LOW  14
+#define LCD_BACKLIGHT_MID  22
+#define LCD_BACKLIGHT_HIGH 23
+
+#endif //P8WATCH 
+#ifdef SN80
+// SN80########################################################################################
+//display
+#define LCD_SCK      2
+#define LCD_SDI      13//sn80 ??
+#define LCD_CS       25
+#define LCD_RESET    26
+#define LCD_RS       18
+#define LCD_DET      9
+
+//Flash Chip
+#define SPI_SCK      2
+#define SPI_MOSI     3
+#define SPI_MISO     4
+#define SPI_CE       5
+
+//Touchscreen
+#define TP_SDA       6
+#define TP_SCL       7
+#define TP_RESET     10 //sn80
 #define TP_INT       28
 
 //Accl Sensor
@@ -45,7 +99,7 @@
 
 //Battery
 //#define CHARGE_INDICATION 12 // Pinetime
-#define CHARGE_INDICATION -1  // P8 Watch
+#define CHARGE_INDICATION -1  // P8 Watch// SN80??
 #define POWER_INDICATION  19
 #define BATTERY_VOLTAGE   31
 #define POWER_CONTROL 24
@@ -53,18 +107,17 @@
 //InputOutput
 #define STATUS_LED        27
 #define VIBRATOR_OUT      16
-//#define PUSH_BUTTON_IN    13 // Pinetime
-//#define PUSH_BUTTON_OUT   15 //PinetTime
-#define PUSH_BUTTON_IN      17   // P8 Watch15//P22
-#define PUSH_BUTTON_OUT   -1   // P8 Watch
+#define PUSH_BUTTON_IN    9 // sn80
+#define PUSH_BUTTON_OUT   17 // sn80
 
 //Backlight
 #define LCD_BACKLIGHT_LOW  14
 #define LCD_BACKLIGHT_MID  22
 #define LCD_BACKLIGHT_HIGH 23
 
-#else //P8WATCH 
+#endif // SN80
 
+#ifdef PineTime
 // Pinetime########################################################################################
 //display
 #define LCD_SCK      2
@@ -83,8 +136,7 @@
 //Touchscreen
 #define TP_SDA       6
 #define TP_SCL       7
-#define TP_RESET     10 //PinetTime
-//#define TP_RESET       13 // P8 Watch
+#define TP_RESET     10 
 #define TP_INT       28
 
 //Accl Sensor
@@ -99,8 +151,7 @@
 #define HRS3300_TEST      30
 
 //Battery
-#define CHARGE_INDICATION 12 // Pinetime
-//#define CHARGE_INDICATION -1  // P8 Watch
+#define CHARGE_INDICATION 12 
 #define POWER_INDICATION  19
 #define BATTERY_VOLTAGE   31
 #define POWER_CONTROL 24
@@ -108,10 +159,8 @@
 //InputOutput
 #define STATUS_LED        27
 #define VIBRATOR_OUT      16
-#define PUSH_BUTTON_IN    13 // Pinetime
-#define PUSH_BUTTON_OUT   15 //PinetTime
-//#define PUSH_BUTTON_IN    17   // P8 Watch
-//#define PUSH_BUTTON_OUT   -1   // P8 Watch
+#define PUSH_BUTTON_IN    13 
+#define PUSH_BUTTON_OUT   15 
 
 //Backlight
 #define LCD_BACKLIGHT_LOW  14
@@ -119,6 +168,7 @@
 #define LCD_BACKLIGHT_HIGH 23
 
 
-#endif // P8WATCH
+#endif // PineTime
+
 
 #endif // _PINOUT_
