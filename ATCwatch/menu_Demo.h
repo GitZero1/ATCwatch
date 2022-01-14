e55e37aaca74b2f89c1e444c25c9ea5c52940b8b
GitZerone <zeroonegames89@gmail.com>
2022-01-14 11:52:43 -0500

    move to demo

--- ATCwatch/menu_Demo.h
+++ ATCwatch/menu_Demo.h
@@ -31,18 +31,20 @@ class DemoScreen : public Screen
   public:
     virtual void pre()
     {
-      static const char * btns[] = {"Yes", "No", ""};
-      lv_obj_t * msgbox1 = lv_msgbox_create(lv_scr_act(), NULL);
-      lv_msgbox_set_text(msgbox1, "Reboot ?");
-      lv_msgbox_add_btns(msgbox1, btns);
-      lv_obj_set_width(msgbox1, 200);
-      lv_obj_set_event_cb(msgbox1, event_handler);
-      lv_obj_align(msgbox1, NULL, LV_ALIGN_CENTER, 0, 0);
+      
+      time_data = get_time();
+      demo_img = lv_img_create(lv_scr_act(),nullptr);
+      lv_img_set_src(demo_img, &ItestImg);
+      lv_img_set_pivot(demo_img, 9, 120);
+      //lv_img_set_zoom(demo_img,1024);
+      lv_obj_align(demo_img, nullptr, LV_ALIGN_CENTER, 0, -111);
     }
 
 
     virtual void main()
     {
+      time_data = get_time();
+      lv_img_set_angle(demo_img, time_data.sec*6*10);
     }
 
     virtual void up()
@@ -58,16 +60,9 @@ class DemoScreen : public Screen
     }
 
     private:
+    lv_obj_t *demo_img;
+    time_data_struct time_data;
 
-    static void event_handler(lv_obj_t * obj, lv_event_t event)
-    {
-      if (event == LV_EVENT_VALUE_CHANGED) {
-        if ("Yes" == lv_msgbox_get_active_btn_text(obj))
-          set_reboot();
-        else if ("No" == lv_msgbox_get_active_btn_text(obj))
-          set_last_menu();
-      }
-    }
 };
 
 DemoScreen demoScreen;
